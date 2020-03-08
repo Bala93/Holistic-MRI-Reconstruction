adc6ce91080c3d67009195feb384405a39e0a673
bala93 <balamuralim.1993@gmail.com>
2020-03-08 17:19:01 +0530

    remove function call

--- deep_cascade_cnn/mail_sys.py
+++ deep_cascade_cnn/mail_sys.py
@@ -23,6 +23,3 @@ def send_mail(exp_info,metric):
     s.quit()
 
     return 
-
-
-send_mail('Dc-CNN','HFN = 0.333 +/- 0.1368 MSE = 0.0005695 +/- 0.0004549 NMSE = 0.004795 +/- 0.003832 PSNR = 32.75 +/- 3.286 SSIM = 0.9195 +/- 0.0425')
