9b61c8b188e82226e80a04601f6675b86ae41390
bala93 <balamuralim.1993@gmail.com>
2020-04-01 12:17:48 +0530

    cleanup zf data code

--- dataset_creation/create_zf_data.sh
+++ dataset_creation/create_zf_data.sh
@@ -1,57 +1,11 @@
-#<<2x
-acc_factor='2x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
-echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} &
-#2x
-
-
-#<<2.5x
-acc_factor='2_5x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
-echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} &
-#2.5x
-
-
-#<<3.3x
-acc_factor='3_3x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
-echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} &
-#3.3x
-
-#<<4x
-acc_factor='4x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
-echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} &
-#4x
-
-#<<5x
+base_path='/data/balamurali'
 acc_factor='5x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
-echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} &
-#5x
-
-#<<8x
-acc_factor='8x'
-dataset_type='cardiac'
-validation_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/validation/acc_'${acc_factor}
-zf_save_path='/media/htic/NewVolume1/murali/MR_reconstruction/datasets/'${dataset_type}'/zf_data/acc_'${acc_factor}
+dataset_type='kirby90'
+mask_type='gaussian'
+model_type='zf'
+validation_path=${base_path}'/datasets/'${dataset_type}'/'${mask_type}'/validation/acc_'${acc_factor}
+zf_save_path=${base_path}'/experiments/'${dataset_type}'/'${mask_type}'/acc_'${acc_factor}'/'${model_type}
 echo python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
 python create_zf_data.py --validation_path ${validation_path} --zf_save_path ${zf_save_path} --acc_factor ${acc_factor} --dataset_type ${dataset_type} 
-#8x
 
 
