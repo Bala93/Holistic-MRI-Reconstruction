bcc6f03d74ddd8d70a2fb82db47fafa28707ec7b
bala93 <balamuralim.1993@gmail.com>
2020-04-07 08:57:33 +0530

    Add rsn in rdn

---
+++ deep_cascade_rsn_rdn/report_collect_kirby.sh
@@ -0,0 +1,36 @@
+
+MODEL='deep-cascade-rdn'
+
+BASE_PATH='/data/balamurali'
+DATASET_TYPE='kirby90'
+#MASK_TYPE='gaussian'
+MASK_TYPE='cartesian'
+
+echo ${MODEL}
+echo ${DATASET_TYPE}
+echo ${MASK_TYPE}
+
+#<<ACC_FACTOR_4x
+ACC_FACTOR='4x'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/'${MASK_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/report.txt'
+echo ${ACC_FACTOR}
+cat ${REPORT_PATH}
+echo "\n"
+#ACC_FACTOR_4x
+
+#<<ACC_FACTOR_5x
+ACC_FACTOR='5x'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/'${MASK_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/report.txt'
+echo ${ACC_FACTOR}
+cat ${REPORT_PATH}
+echo "\n"
+#ACC_FACTOR_5x
+
+#<<ACC_FACTOR_8x
+ACC_FACTOR='8x'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/'${MASK_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/report.txt'
+echo ${ACC_FACTOR}
+cat ${REPORT_PATH}
+echo "\n"
+#ACC_FACTOR_8x
+
