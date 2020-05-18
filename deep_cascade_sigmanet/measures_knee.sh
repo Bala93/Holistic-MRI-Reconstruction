0ed3339caa7b9968a02583438e6529d0db53df21
bala93 <balamuralim.1993@gmail.com>
2020-05-18 18:42:57 +0530

    Add sigmanet didn code base to parallel imaging

---
+++ deep_cascade_sigmanet/measures_knee.sh
@@ -0,0 +1,28 @@
+MODEL='vs-net-deep-cascade'
+DATASET_TYPE='knee-mc-coronal-pd'
+#DATASET_TYPE='knee-mc-coronal-pd-fs'
+#DATASET_TYPE='knee-mc-axial-t2'
+BASE_PATH='/media/htic/NewVolume1/murali/MR_reconstruction'
+#MASK_TYPE='gaussian'
+#MASK_TYPE='cartesian'
+
+
+ACC_FACTOR='5x'
+#TARGET_PATH=${BASE_PATH}'/datasets/'${DATASET_TYPE}'/validation/'
+TARGET_PATH='/media/htic/NewVolume5/knee_mri_vsnet_globus/coronal_pd_h5/validation'
+PREDICTIONS_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/results'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}
+echo ${TARGET_PATH}
+echo ${PREDICTIONS_PATH}
+echo ${REPORT_PATH}
+python measures_csv.py --target-path ${TARGET_PATH} --predictions-path ${PREDICTIONS_PATH} --report-path ${REPORT_PATH}
+
+
+
+<<ACC_FACTOR_8x
+ACC_FACTOR='8x'
+TARGET_PATH=${BASE_PATH}'/datasets/'${DATASET_TYPE}'/'${MASK_TYPE}'/validation/acc_'${ACC_FACTOR}
+PREDICTIONS_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/'${MASK_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/results'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/'${MASK_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/results'
+python measures_csv.py --target-path ${TARGET_PATH} --predictions-path ${PREDICTIONS_PATH} --report-path ${REPORT_PATH}
+#ACC_FACTOR_8x
---
+++ deep_cascade_sigmanet/report_collect_knee.sh
@@ -0,0 +1,16 @@
+MODEL='sigmanet-deep-cascade'
+BASE_PATH='/media/htic/NewVolume1/murali/MR_reconstruction'
+DATASET_TYPE='knee-mc-coronal-pd'
+
+echo ${MODEL}
+echo ${DATASET_TYPE}
+echo ${MASK_TYPE}
+
+#<<ACC_FACTOR_4x
+ACC_FACTOR='4x'
+REPORT_PATH=${BASE_PATH}'/experiments/'${DATASET_TYPE}'/acc_'${ACC_FACTOR}'/'${MODEL}'/report.txt'
+echo ${ACC_FACTOR}
+cat ${REPORT_PATH}
+echo "\n"
+#ACC_FACTOR_4x
+
